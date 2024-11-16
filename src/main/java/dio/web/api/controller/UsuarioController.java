44ff23e6112241029213d7d4a8ca73f991524de8
Adorno Felipe <felipeoliveiraadorno77@hotmail.com>
2024-11-16 16:13:07 -0300

    Segurança

--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -1,7 +1,7 @@
 package dio.web.api.controller;
 
-import dio.web.api.model.Usuario;
-import dio.web.api.repository.UsuarioRepository;
+import dio.web.api.model.User;
+import dio.web.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,32 +11,35 @@ import java.util.List;
 @RequestMapping("/users")
 public class UsuarioController {
     @Autowired
-    private UsuarioRepository repository;
+    private UserRepository repository;
 
-    @GetMapping()
-    public List<Usuario> getUser(){
+
+    @PostMapping()
+    public void postUser(@RequestBody User user){
+        repository.save(user);
+
+    }
+
+     @GetMapping()
+    public List<User> getUser(){
         return  repository.findAll();
     }
 
     @GetMapping("/{username}")
-    public Usuario getOne(@PathVariable("username") String username){
+    public User getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
     }
 
-    @DeleteMapping("{id}")
-    public void deleteUser(@PathVariable("id")Integer id){
-        repository.deletebById(id);
-    }
+    //@DeleteMapping("{id}")
+   // public void deleteUser(@PathVariable("id")Integer id){
+    //    repository.deletebById(id);
+   // }
 
-    @PostMapping()
-    public void postUser(@RequestBody Usuario usuario){
-        repository.save(usuario);
 
-    }
+   @PutMapping()
+    public void putUser(@RequestBody User user){
+        repository.save(user);
+}
 
-    @PutMapping()
-    public void putUser(@RequestBody Usuario usuario){
-        repository.save(usuario);
 
-    }
 }
--- src/main/java/dio/web/api/doc/SwaggerConfig.java
+++ src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package dio.web.api.doc;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,9 +10,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.service.Contact;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 @Configuration
 @EnableSwagger2
